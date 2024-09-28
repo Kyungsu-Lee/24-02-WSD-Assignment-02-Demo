73e585d293a08fb4ed4757cfd73360d15305f30b
Kyungsu-Lee <lmasi@naver.com>
2024-09-28 13:56:38 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/24-02-WSD-Assignment-02-Demo/', // GitHub Pages 배포용 base 경로 설정
 })
