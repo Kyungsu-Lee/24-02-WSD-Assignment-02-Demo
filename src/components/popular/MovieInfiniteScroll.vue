71c0fb87052fb59796d88016d6a0c398ab419963
Kyungsu-Lee <lmasi@naver.com>
2024-10-20 05:49:23 +0900

    update popular

--- src/components/popular/MovieInfiniteScroll.vue
+++ src/components/popular/MovieInfiniteScroll.vue
@@ -4,9 +4,7 @@
       <div v-for="(movieGroup, index) in visibleMovieGroups" :key="index"
            :class="['movie-row', { 'full': movieGroup.length === rowSize }]">
         <div v-for="movie in movieGroup" :key="movie.id" class="movie-card"
-             @mousedown="toggleWishlist(movie)"
-             @contextmenu.prevent="toggleWishlist(movie)"
-             @touchend="toggleWishlist(movie)"
+             @mouseup="toggleWishlist(movie)"
         >
           <img :src="getImageUrl(movie.poster_path)" :alt="movie.title">
           <div class="movie-title">{{ movie.title }}</div>
